7ec44e95a593ccd53612dce562ce76ca8be93587
Tim <jatimix@gmail.com>
2025-11-15 07:53:56 +0100

    :bug: fix environment not existing on nix

--- config.el
+++ config.el
@@ -43,7 +43,7 @@
 (setq org-directory "~/org/")
 
 (setq doom-env-file "~/.doom.d/environment")
-(doom-load-envvars-file "~/.doom.d/environment")
+;; (doom-load-envvars-file "~/.doom.d/environment")
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
